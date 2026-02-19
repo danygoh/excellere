eecda9ca3d4efe7ad0783536408eeead42ef7a2d
Danny Goh <danny.goh@Mac-mini-M4.local>
2026-02-19 18:53:13 +0000

    Update fonts: Plus Jakarta Sans (UI), Lora (body), Syne (headlines), JetBrains Mono (code)

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link 
-          href="https://fonts.googleapis.com/css2?family=Instrument+Serif:ital@0;1&family=JetBrains+Mono:wght@400;500;600&family=Syne:wght@400;500;600;700&display=swap" 
+          href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;600&family=Lora:ital,wght@0,400;0,500;0,600;1,400&family=Plus+Jakarta+Sans:wght@400;500;600;700&family=Syne:wght@400;500;600;700&display=swap" 
           rel="stylesheet" 
         />
       </head>
